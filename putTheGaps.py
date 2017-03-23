00fb6dedd2e47bbbf93b52f8053b89a9931eb989
RandyRomero <a.ya.miheev@gmail.com>
2017-03-23 16:13:36 +0300

    make function that renames files backwards from last file to file with number that submit user (inclusive)

--- putTheGaps.py
+++ putTheGaps.py
@@ -6,6 +6,14 @@ import os, re
 
 pathToWork = ('.\\test')
 
+
+
+def rename(number):
+	files = os.listdir(pathToWork)
+	for i in range(len(files), number - 1, -1):
+		os.rename(os.path.join(pathToWork, 'file{0:0>3}.txt'.format(i)), os.path.join(pathToWork, 'file{0:0>3}.txt'.format(i + 1)))
+
+
 ########### Ask user about a number of file he wants to substitute ##########
 
 while True:
@@ -13,6 +21,7 @@ while True:
 	if re.search(r'^\d{3}$', number): # only three digits
 		if os.path.exists(os.path.join(pathToWork, 'file{0:0>3}.txt'.format(number))): # check if file with this number exists
 			print('This file exists. Accepted.')
+			rename(int(number))
 			break
 		else:	
 			print('file{0:0>3}.txt doesn\'t exists. Choose another one.\n'.format(number))
