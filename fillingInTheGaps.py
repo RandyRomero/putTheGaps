31a79a4ce1b403f65aa4f9501cd2a7adc742b0be
RandyRomero <a.ya.miheev@gmail.com>
2017-03-31 23:03:23 +0300

    make script to determine the last file by itself whothout asking user

--- fillingInTheGaps.py
+++ fillingInTheGaps.py
@@ -2,22 +2,49 @@
 
 
 '''Program that finds all files with a given prefix, such as spam001.txt, spam002.txt, and so on, in a single folder and locates any gaps in the numbering (such as if there is a spam001.txt and spam003.txt but no spam002.txt). Have the program rename all the later files to close this gap.'''
-import os
+
+import os, logging, re, sys
+
+logging.basicConfig(
+    format = "%(levelname) -1s %(asctime)s line %(lineno)s: %(message)s",
+    level = logging.DEBUG
+)
+logging.disable(logging.CRITICAL)
 
 pathToWork = ('.\\test')
-files = os.listdir(pathToWork)
 allesGut = True
 
+
+def searchBoundaries():
+	files = os.listdir(pathToWork)
+	# gather only files with right mask
+	files[:] = [file for file in files if re.search(r'file(\d{3})\.txt', file)]
+	# gather serial number of these files
+	serialNumbers = []
+	for file in files:
+		serialNum = re.search(r'file(\d{3}).txt', file).group(1)
+		serialNumbers.append(serialNum)
+
+	serialNumbers.sort()		
+	firstNumber = serialNumbers[0]
+	lastNumber = serialNumbers[len(serialNumbers)-1]
+	logging.debug(lastNumber)
+
+	return files, int(firstNumber), int(lastNumber)
+
+files, firstNumber, lastNumber = searchBoundaries()
+
+y = 0
+
 # look for missing files
-for i in range(1, len(files) + 1):
+for i in range(firstNumber, lastNumber + 1):
 	currentFile = os.path.join(pathToWork, 'file{0:0>3}.txt'.format(i))
 	if not os.path.exists(currentFile):
-		print('file{0:0>3}.txt is missing!'.format(i))
+		
 		allesGut = False
-		y = 1
-
+		
 		while True:
-			#loop recusively looks for file with next number and renames it as missing file
+			#loop recusively looks for a file with next number and give it a name of a missing file
 
 			nextFile = os.path.join(pathToWork, 'file{0:0>3}.txt'.format(i + y))
 			
@@ -28,12 +55,15 @@ for i in range(1, len(files) + 1):
 				break
 			else:
 				# if file doesn't exist - increment 'y' in order to look for the file with bigger ordinal number next time
-				if not y > len(files):
+				if i + y < lastNumber:
+					print('file{0:0>3}.txt is missing!'.format(i + y))
 					y += 1
+					logging.debug(y)
 					continue
 				else:
 					print('There are no files to rename anymore')
-					break
-	
+					sys.exit()
+	else:
+		logging.debug('file{0:0>3}.txt is here'.format(i))
 if allesGut:
 	print('All files are there.')
--- putTheGaps.py
+++ putTheGaps.py
@@ -2,39 +2,46 @@
 
 '''Program that can insert gaps into numbered files so that a new file can be added.'''
 
-import os, re
+import logging, os, re
+
+logging.basicConfig(
+	format = "%(levelname) -1s %(asctime)s line %(lineno)s: %(message)s",
+	level = logging.DEBUG
+)
+
+logging.disable(logging.CRITICAL)
+
 
 pathToWork = ('.\\test')
 
+def lastFileNumber(files):
+	
+	# Read files, check which one is last, pass number of last file to loop which start to rename files from the last one
 
-def lastFileNumber():
-	while True:
-		lastFile = input('Please write down number of the very last file (e.g. 004): ')
-		if re.search(r'^\d{3}$', lastFile): # only three digits
-			if os.path.exists(os.path.join(pathToWork, 'file{0:0>3}.txt'.format(lastFile))): # check if file with this number exists
-				print('This file exists. Accepted.')
-				return int(lastFile)
-			else:	
-				print('file{0:0>3}.txt doesn\'t exists. Choose another one.\n'.format(lastFile))
-				continue
-		else:
-			print('Incorrect input. Try again.\n')
-			continue
-
+	serialNumbers = []
+	for file in files:
+		if re.search(r'(\d{3})', file).group(1):
+			serialNumbers.append(file)
+		
+	serialNumbers.sort()
+	lastFile = int(re.search(r'(\d{3})', serialNumbers[len(serialNumbers) - 1]).group(1))
+	logging.debug('lastFile is ' + str(lastFile))
+	return lastFile
 		
 def rename(fileNumber, number):
 	
 	# The whole functuon runs like this: it takes last file (which equal length of file list), e.g. file047.txt, and rename it as file with ordered number one step higher - file048.txt. It goes from very last number to number which user chosed (inclusive).
 	
 	files = os.listdir(pathToWork)
-	lastFile = len(files)
-	end = False
+	lastFile = lastFileNumber(files)
+	end = False # What the heck is it? 
 	while end == False:
 		# print('last file is ' + str(lastFile))
 		
 		for i in range(lastFile, fileNumber - 1, -1): #loop runs backwards 
 			
 			currentFile = os.path.join(pathToWork, 'file{0:0>3}.txt'.format(i))
+			# logging.debug(currentFile)
 			nextFile = os.path.join(pathToWork, 'file{0:0>3}.txt'.format(i + number))
 			try:
 				os.rename(currentFile, nextFile)
@@ -43,10 +50,12 @@ def rename(fileNumber, number):
 					return
 			except FileExistsError:
 				print('File already exists\n')
-				lastFile = lastFileNumber()
+				lastFile = lastFileNumber(files) #TODO now it is seems redundant
 				break
+			except FileNotFoundError:
+				print('Can\'t rename ' + currentFile + '. File is missing.')
+
 			
-				
 ########### Ask user about a number of file he wants to substitute ##########
 
 while True:
